99ed3cfae09873ad0dc67a9f2bbcff47f1057aae
Betty Jimenez <bettyjimenez3010@gmail.com>
2021-08-24 12:02:56 -0400

    :lipstick: add: styles files added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 // import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="text-gray-400 btn">
-     Hola
+    <div className="text-gray-400 bg-gray-500">
+      Hola
+      <div className="text-pink-500 border border-green-400 rounded-md  btn">Rossana</div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,47 @@ module.exports = {
   purge: ["./components/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
   darkMode: false,
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#000000",
+      white: "#ffffff",
+      success: "#44d64b",
+      gray: {
+        100: "#fafafa",
+        200: "#f4f4f4",
+        300: "#e6ebf1",
+        400: "#eaeaea",
+        500: "#cccccc",
+        600: "#9aabb5",
+        700: "#999999",
+        800: "#43484d",
+        900: "#3c3c3c",
+      },
+      blue: {
+        100: "#fff8f9",
+        200: "#5089C6", // Light button
+        500: "#035397",
+        700: "#001E6C", // gradient
+      },
+      purple: {
+        100: "#f1e8ff", // Light button
+        500: "#200844",
+      },
+      red: {
+        500: "#e32726",
+      },
+      yellow: {
+        100: "#fffff0",
+        200: "#fefcbf",
+        500: "#FFAA4C",
+
+      },
+    },
   },
   variants: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
-
+  plugins: [],
+};
