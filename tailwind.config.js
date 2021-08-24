409b56fbbb3c0b68e966b4a80371f19165cb0b0b
Betty Jimenez <bettyjimenez3010@gmail.com>
2021-08-23 22:11:12 -0400

    :tada: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ["./components/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
+
